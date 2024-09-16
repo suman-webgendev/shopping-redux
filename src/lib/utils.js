0ec9ea74671aaf1e7beaa9a97dabf53b37e2b8c5
suman <suman.webgen@gmail.com>
2024-09-16 10:57:09 +0530

    feat: added jsdoc for the formatPrice function

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -5,6 +5,11 @@ export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
 
+/**
+ * This function takes price amount as a parameter and returns formatted price in USD
+ * @param {number} price
+ * @returns {string}
+ */
 export const formatPrice = (price) => {
   return Intl.NumberFormat("en-US", {
     style: "currency",
